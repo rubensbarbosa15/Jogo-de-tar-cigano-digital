d37f5e5a24230412b80eca69d0ec1d0addbf078c
rubensbarbosa15 <94624594+rubensbarbosa15@users.noreply.github.com>
2023-09-13 21:28:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request, jsonify
-import psycopg2
+from flask_mysqldb import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
 
-# Conexão com o banco de dados PostgreSQL
-db_connection = psycopg2.connect(
-    host="localhost",  # O endereço do contêiner Docker do banco de dados
-    database="tarot_db",  # O nome do banco de dados
-    user="tarot_user",  # O usuário do banco de dados
-    password="tarot_password"  # A senha do usuário do banco de dados
-)
+#app.template_folder = "templates" 
 
-db_cursor = db_connection.cursor()
+app.config['MYSQL_HOST'] = '172.19.0.3'  # Altere para o endereço IP correto
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'tarot_password'
+app.config['MYSQL_DB'] = 'tarot_db'
+
+mysql.init_app(app)
 
 @app.route('/')
+
 def index():
     return render_template('index.html')
 
@@ -22,17 +23,47 @@ def cadastrar():
     nome = request.form.get('name')
     email = request.form.get('email')
 
-    # Inserir o usuário no banco de dados
-    inserir_usuario(nome, email)
+    conn = mysql.connect()
+    cursor = conn.cursor()
 
-    mensagem = f'Boas-vindas, {nome}! Seu cadastro foi realizado com sucesso.'
+    try:
+        cursor.execute("INSERT INTO users (nome, email) VALUES (%s, %s)", (nome, email))  # Alterado para 'nome'
+        conn.commit()
+        mensagem = f'Boas-vindas, {nome}! Seu cadastro foi realizado com sucesso.'
+    except Exception as e:
+        conn.rollback()
+        mensagem = f'Erro ao cadastrar usuário: {str(e)}'
+    finally:
+        cursor.close()
+        conn.close()
 
     return jsonify({"mensagem": mensagem})
 
-def inserir_usuario(nome, email):
-    insert_query = "INSERT INTO users (name, email) VALUES (%s, %s)"
-    db_cursor.execute(insert_query, (nome, email))
-    db_connection.commit()
+@app.route('/listar', methods=['GET'])
+def listar():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM users")
+        resultados = cursor.fetchall()
+        lista_usuarios = []
+
+        for resultado in resultados:
+            usuario = {
+                'id': resultado[0],
+                'nome': resultado[1],  # Alterado para 'nome'
+                'email': resultado[2]
+            }
+            lista_usuarios.append(usuario)
+
+    except Exception as e:
+        mensagem = f'Erro ao listar usuários: {str(e)}'
+    finally:
+        cursor.close()
+        conn.close()
+
+    return jsonify({"usuarios": lista_usuarios})
 
 if __name__ == "__main__":
     app.run(debug=True)
